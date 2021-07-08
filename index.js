2ca796dfdadb46c38b4bebf255520fb792df4aa9
Gabriel Gutierrez <poo328@my.utsa.edu>
2021-07-08 00:35:44 -0500

    added prompts for inquirer to build team

---
+++ index.js
@@ -0,0 +1,142 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const jest = require('jest');
+const path = require('path');
+
+
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
+const team = [];
+
+// prompts for Engineer info
+function addEngineer() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: 'What is the name of your Engineer?',
+        },
+        {
+            type: 'input',
+            name: 'engineerId',
+            message: 'What is the ID Number of your Engineer?',
+        },
+        {
+            type: 'input',
+            name: 'engineerEmail',
+            message: 'What is the email address of your Engineer?',
+        },
+        {
+            type: 'input',
+            name: 'engineerGithub',
+            message: 'What is the Github Username of your Engineer?',
+        },
+    ]).then(answers => {
+        const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+        team.push(engineer);
+        buildTeam();        
+    }); 
+}
+
+// prompts for Intern info
+function addIntern() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'internName',
+            message: 'What is the name of your Intern?',
+        },
+        {
+            type: 'input',
+            name: 'internId',
+            message: 'What is the ID Number of your Intern?',
+        },
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: 'What is the email address of your Intern?',
+        },
+        {
+            type: 'input',
+            name: 'internSchool',
+            message: 'What school is your Intern attending?',
+        },
+    ]).then(answers => {
+        const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+        team.push(intern);
+        buildTeam();
+    });    
+}
+
+// prompts for Manager info
+function addManager() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'managerName',
+            message: 'What is the name of your Manager?',
+        },
+        {
+            type: 'input',
+            name: 'managerId',
+            message: 'What is the ID Number of your Manager?',
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: 'What is the email address of your Manager?',
+        },
+        {
+            type: 'input',
+            name: 'managerOfficenumber',
+            message: 'What is the Office Number of your Manager?',
+        },
+    ]).then(answers => {
+        const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+        team.push(manager);
+        buildTeam();
+    });    
+}
+
+// function to start/continue adding members to the team
+function buildTeam() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'employeeType',
+            message: 'What type of employee would you like to add to your team?',
+            choices: [
+                "Manager",
+                "Engineer",
+                "Intern",
+                "Finalize Team"
+            ]
+        }
+    ]).then(userChoice => {
+        switch (userChoice.employeeType) {
+            case "Manager":
+                addManager();
+                break;
+            case "Engineer":
+                addEngineer();
+                break;
+            case "Intern":
+                addIntern();
+                break;
+            case "Finalize Team":
+                renderHTML();
+                break;
+            default:
+                renderHTML();
+        }
+    })
+
+}
+
+function renderHTML() {
+    console.log("Building Your Team Of All-Stars");
+    fs.writeFile(outputPath, render(team), "utf-8");
+}
